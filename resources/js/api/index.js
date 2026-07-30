a7f05e8ca8b0a568815d063de0874618c10fc94e
Damaso-Web <anabua70@gmail.com>
2026-07-30 11:22:50 +0800

    Update API URL to Render backend and build frontend

--- resources/js/api/index.js
+++ resources/js/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://icare-backend-5jwe.onrender.com/api',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
