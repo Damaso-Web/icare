f849703da68e867ab5dff9be2ad8ee36ae0ec2f0
Damaso-Web <anabua70@gmail.com>
2026-09-04 08:56:49 +0800

    Fix faculty directory route detection

--- resources/js/layouts/MainLayout.vue
+++ resources/js/layouts/MainLayout.vue
@@ -182,7 +182,7 @@ const menuItems = computed(() => {
       section: null,
     },
     {
-      name:    'users',
+      name:    'faculty-directory',
       label:   'Faculty',
       icon:    '<path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/>',
       roles:   ['admin'],
