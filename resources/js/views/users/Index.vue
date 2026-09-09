ffbb7bf1e425ebab5ff91c4df724842b66079662
Damaso-Web <anabua70@gmail.com>
2026-09-10 00:05:58 +0800

    Fix missing isValidEmail import causing employee add crash

--- resources/js/views/users/Index.vue
+++ resources/js/views/users/Index.vue
@@ -303,7 +303,7 @@ import { userAPI } from '../../api/index';
 import { useAuthStore } from '../../stores/auth';
 import { COLLEGES } from '../../constants/colleges';
 import { DEPARTMENTS_BY_COLLEGE } from '../../constants/departments';
-import { onlyLetters, onlyLettersStrict, onlyDigits, contactNumberInput, isValidPHContact, safeSearchInput, blockSpecialKeypress } from '../../utils/validators';
+import { onlyLetters, onlyDigits, contactNumberInput, isValidEmail, isValidPHContact, safeSearchInput, blockSpecialKeypress } from '../../utils/validators';
 
 const route      = useRoute();
 const toast      = inject('toast');
@@ -409,7 +409,6 @@ function openEdit(u) {
 
 async function saveUser() {
   formError.value = '';
-  console.log('Contact number value:', userForm.value.contact_number);
   if (!auth.isAdmin) {
     formError.value = 'Please fill in all required fields.';
     return;
@@ -427,7 +426,6 @@ async function saveUser() {
     return;
   }
   if (userForm.value.contact_number && !isValidPHContact(userForm.value.contact_number)) {
-    console.log('Failed PH contact check:', userForm.value.contact_number);
     formError.value = 'Contact number must start with 09 and be 11 digits long.';
     return;
   }
